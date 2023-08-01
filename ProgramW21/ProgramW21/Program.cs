f4ec66c9b64e096abb9d012e8766d819d47a55f4
seb04it <seb04it@gmail.com>
2023-08-01 13:07:14 +0200

    Dzień 4

---
+++ ProgramW21/ProgramW21/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string name = "Seba";
+string gender = "Male";
+int age = 18;
+
+if( gender == "Female" && age < 30 )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(gender == "Male" && age < 18 )
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if(name == "Ewa" && age == 33 )
+{
+    Console.WriteLine("Ewa lat 33" );
+}
+else
+{
+    Console.WriteLine("Parametry nie pasują do żadnych kategorii");
+}
